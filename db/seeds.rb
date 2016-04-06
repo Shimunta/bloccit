8c51221967e4a8a6c4a9e37f056108badc1d619a
Daniel Lisa <danlisa89@gmail.com>
2016-04-06 00:38:32 -0400

    Assigment 33 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'random_data'
-
+# Create Posts
 50.times do
   Post.create!(
     title: RandomData.random_sentence,
@@ -7,14 +7,22 @@ require 'random_data'
   )
 end
 posts = Post.all
-
+# Create Comments
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
-
+#Create Questions
+50.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  )
+end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
