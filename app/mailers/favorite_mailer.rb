dd54698623b01c18a0d3c02d13ed02c690742651
Daniel Lisa <danlisa89@gmail.com>
2016-04-21 13:02:21 -0400

    assignment 44 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "youremail.com"
+  default from: "danlisa89@gmail.com"
 
   def new_comment(user, post, comment)
 
@@ -14,5 +14,16 @@ class FavoriteMailer < ApplicationMailer
 
 
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "Thanks for favoriting, #{post.title}!")
   end
 end
